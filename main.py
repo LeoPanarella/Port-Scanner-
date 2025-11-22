c1426aa7f3eb0577de4ef3d9631368fce848507f
leonardopanarellaneto <leopanarella10@gmail.com>
2025-11-21 21:23:46 -0300

    first project

---
+++ main.py
@@ -0,0 +1,45 @@
+import socket
+import threading
+from queue import Queue
+
+target = "127.0.0.1" # The target IP address for the scan; 'localhost', or the local machine.
+queue = Queue()
+open_ports = []
+
+def portscan(port):
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.settimeout(0.3)
+        sock.connect((target, port))
+        sock.close
+        return True
+    except:
+        return False 
+
+def fill_queue(port_list):
+    for port in port_list:
+        queue.put(port)
+
+def worker():
+    while not queue.empty():
+        port = queue.get()
+        if portscan(port):
+            print("port {} is open!".format(port))
+            open_ports.append(port)
+
+port_list = range(1,1024)
+fill_queue(port_list)
+
+thread_list = []
+
+for t in range(500):
+    thread = threading.Thread(target=worker)
+    thread_list.append(thread)
+
+for thread in thread_list:
+    thread.start()
+
+for thread in thread_list:
+    thread.join()
+
+print("open ports are : ",open_ports)
